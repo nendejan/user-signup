2bf96f33cce1314107d04fc4b433116e56811a65
Nick Endejan <nendejan@gmail.com>
2017-03-30 11:46:04 -0500

    set up html elements for input areas and button

--- main.py
+++ main.py
@@ -15,11 +15,76 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
 
-class MainHandler(webapp2.RequestHandler):
+indexHeader = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User-Signup</title>
+</head>
+<body>
+    <h1>Signup</h1>
+"""
+indexFooter = """
+</body>
+</html>
+"""
+
+
+class Index(webapp2.RequestHandler):
+
+    """Handles requests coming in to '/' """
     def get(self):
-        self.response.write('Hello world!')
+        indexForm = """
+        <form method ="post">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label for="username">Username</label>
+                        </td>
+                        <td>
+                            <input name="username" type="text" value required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="password">Password</label>
+                        </td>
+                        <td>
+                            <input name="password" type="password" value required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="verifyPassword">Verify Password</label>
+                        </td>
+                        <td>
+                            <input name="verifyPassword" type="password" value required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="email">Email</label>
+                        </td>
+                        <td>
+                            <input name="email" type="text" value required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="submit">
+        </form>
+        """
+
+        formContent = indexHeader + indexForm + indexFooter
+        self.response.write(formContent)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index)
 ], debug=True)
