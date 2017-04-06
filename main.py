724a1a25836e26f729af9bd08fa0cae9032d9d29
Nick Endejan <nendejan@gmail.com>
2017-04-05 23:16:20 -0500

    almost finished

--- main.py
+++ main.py
@@ -14,8 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+ #TODO: impliment regular expressions for input (bottom of udacity page)
+ #TODO: multiple errors show all their messages on the same redirect
+ #TODO: preserve user imput through redirect (string substitution?
+
+
+
 import webapp2
 import cgi
+import re
 
 indexHeader = """
 <!DOCTYPE html>
@@ -24,6 +32,9 @@ indexHeader = """
     <title>User-Signup</title>
     <style>
         span.error {
+            color:red;
+        }
+        div.error {
             color:red;
         }
     </style>
@@ -37,101 +48,125 @@ indexFooter = """
 """
 
 
+
+
+form = """
+<form method ="post">
+    <table>
+        <tbody>
+            <tr>
+                <td>
+                    <label for="username">Username</label>
+                </td>
+                <td>
+                    <input name="username" type="text" value="%(username)s">
+                    <span class='error'>%(userNameErrorMsg)s</span>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="password">Password</label>
+                </td>
+                <td>
+                    <input name="password" type="password" value required value="%(password)s">
+                    <span class='error'>%(passwordErrorMsg)s</span>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="verifyPassword">Verify Password</label>
+                </td>
+                <td>
+                    <input name="verifyPassword" type="password" value required value="%(verifyPassword)s">
+                    <span class='error'>%(verifyPasswordErrorMsg)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email (optional)</label>
+                </td>
+                <td>
+                    <input name="email" type="text" value="%(email)s">
+                    <span class="error">%(emailErrorMsg)s</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <input type="submit">
+</form>"""
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+#no spaces, no characters(?), 3<
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PASS_RE.match(password)
+#3<
+MAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return MAIL_RE.match(email)
+
+def passwordsMatch(password, verifyPassword):
+    if password == verifyPassword:
+        passwordVerified = True
+    else:
+        passwordVerified = False
+
+
+formContent = indexHeader + form + indexFooter
 class Index(webapp2.RequestHandler):
 
-    """Handles requests coming in to '/' """
     def get(self):
 
+        self.write_form_includes_Errors()
 
-        userNameForm = """
-        <form method ="post">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <label for="username">Username</label>
-                        </td>
-                        <td>
-                            <input name="username" type="text" value required>"""
-        userNameErrorSpan = ""
-        userNameErrorMsg = self.request.get('userNameError')
-        if userNameErrorMsg:
-            userNameErrorSpan = "<span class='error'>" + cgi.escape(userNameErrorMsg) + "</span>"
+    def write_form_includes_Errors(self, userNameErrorMsg="", passwordErrorMsg ="", emailErrorMsg="", verifyPasswordErrorMsg="", username="", password="", verifyPassword="", email=""):
 
-        passwordForm = """
-
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <label for="password">Password</label>
-                        </td>
-                        <td>
-                            <input name="password" type="password" value required>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <label for="verifyPassword">Verify Password</label>
-                        </td>
-                        <td>
-                            <input name="verifyPassword" type="password" value required>"""
-        passwordErrorSpan = ""
-        passwordErrorMsg = self.request.get('passwordError')
-        if passwordErrorMsg:
-                passwordErrorSpan = "<span class='error'>" + cgi.escape(passwordErrorMsg) + "</span>"
-        emailForm = """
-
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email">Email (optional)</label>
-                        </td>
-                        <td>
-                            <input name="email" type="email">
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <input type="submit">
-        </form>
-        """
-#must have seperate error castings so that the proper error lands in proper location?
+        self.response.out.write(formContent % {"userNameErrorMsg": userNameErrorMsg, "passwordErrorMsg": passwordErrorMsg, "emailErrorMsg": emailErrorMsg, "verifyPasswordErrorMsg": verifyPasswordErrorMsg, "username": cgi.escape(username), "password": cgi.escape(password), "verifyPassword": cgi.escape(verifyPassword), "email": cgi.escape(email)})
 
 
-
-        formContent = indexHeader + userNameForm + userNameErrorSpan + passwordForm + passwordErrorSpan + emailForm + indexFooter
-        self.response.write(formContent)
-    def post(self):
+    def post(self, userNameErrorMsg="", passwordErrorMsg ="", emailErrorMsg="", verifyPasswordErrorMsg="", username="", password="", verifyPassword="", email=""):
         #looks inside the request to see what the user typed into form
 
         user_Name = cgi.escape(self.request.get("username"))
-
-        if " " in user_Name:
-            self.redirect("/?userNameError= Please enter valid Username that contains no spaces.")
-
-
-
         user_Email = cgi.escape(self.request.get("email"))
-
         user_Password = cgi.escape(self.request.get("password"))
-
         user_VerifyPassword = cgi.escape(self.request.get("verifyPassword"))
 
+        validUsername = valid_username(user_Name)
+        validEmail = valid_email(user_Email)
+        validPassword = valid_password(user_Password)
+
+
+        if validUsername == None:
+            userNameErrorMsg = "Please enter valid username."
+        if validEmail == None:
+            emailErrorMsg = "Please enter valid email."
+        if validPassword == None:
+            passwordErrorMsg = "Please enter valid password."
         if user_Password != user_VerifyPassword:
-            self.redirect('/?passwordError= Passwords must match.')
+            verifyPasswordErrorMsg = "Passwords must match."
 
-        postHeader = "<h1>Welcome, " + user_Name + "!</h1>"
+        if validUsername == None or validEmail == None or validPassword == None or user_Password != user_VerifyPassword:
 
-        self.response.write(postHeader)
+            self.response.out.write(formContent % {"userNameErrorMsg": userNameErrorMsg, "passwordErrorMsg": passwordErrorMsg, "emailErrorMsg": emailErrorMsg, "verifyPasswordErrorMsg": verifyPasswordErrorMsg, "username": cgi.escape(user_Name), "password": cgi.escape(password), "verifyPassword": cgi.escape(verifyPassword), "email": cgi.escape(user_Email)})
+
+        else:
+            self.redirect("/welcome?username="+user_Name)
+
+class WelcomeHandler(webapp2.RequestHandler):
+
+#LAST STEP: cant get "user_Name" into html but its getting the correct value in urlbar
+    def get(self):
+        postHeader = "<h1>Welcome, " + username + "!</h1>"
+        self.response.out.write(postHeader)
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', Index)
+    ('/', Index),
+    ('/welcome', WelcomeHandler,)
 ], debug=True)
